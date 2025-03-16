48d50efbb097822ab3d62e993593cbcfe6d0fb81
johnsulf <erlendjohns@gmail.com>
2025-03-16 19:35:06 +0100

    ran format script

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 
-export interface IFooterProps {
-}
+export interface IFooterProps {}
 
 export default class Footer extends React.Component<IFooterProps> {
   public render() {
     return (
-      <footer className='bg-dark text-light text-center p-3'>
+      <footer className="bg-dark text-light text-center p-3">
         <p>Copyright 2025</p>
       </footer>
     );
